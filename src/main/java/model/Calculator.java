861d09e018c75bfe5c81966a9a6713811d588729
MatteoGaloni <mtgaloni@gmail.com>
2023-11-06 15:58:00 +0100

    add CalculatorTest

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -1,2 +1,30 @@
-package model;public class Calculator {
+package model;
+
+import exceptions.DividendIsZeroException;
+
+public interface Calculator {
+//    ***ATTRIBUTES***
+
+//    ***CONSTRUCTORS**
+
+//    ***METHODS***
+    public static float add(float num1, float num2){
+        return num1 + num2;
+    }
+    public static float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+    public static float divide(float num1, float num2) throws DividendIsZeroException{
+        if (num2 == 0){
+            throw new DividendIsZeroException();
+        }
+        return num1 / num2;
+    }
+    public static float multiply(float num1, float num2){
+        return num1 * num2;
+    }
+
+
 }
+
+
--- src/test/java/model/CalculatorTest.java
+++ src/test/java/model/CalculatorTest.java
@@ -1,4 +1,51 @@
+package model;
+
+import exceptions.DividendIsZeroException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-  
+
+    @Test
+    @DisplayName("Check Addition")
+    void add() {
+        assertEquals(10, Calculator.add(5,5));
+    }
+
+    @Test
+    @DisplayName("Check Subtraction")
+    void subtract() {
+        long seed = 6789L;
+        Random random = new Random(seed);
+        for (int i = 0; i < 100; i++) {
+            float num1 = random.nextFloat(0,101);
+            float num2 = random.nextFloat(0,101);
+            float result = num1 - num2;
+            assertEquals(result, Calculator.subtract(num1, num2));
+        }
+    }
+
+    @Test
+    @DisplayName("Check Division")
+    void divide() {
+        assertEquals(10, Calculator.divide(100,10));
+    }
+
+    @Test
+    @DisplayName("Check Multiplication")
+    void multiply() {
+        assertEquals(10, Calculator.multiply(5,2));
+    }
+
+    @Test
+    @DisplayName("throws DividendIsZero")
+    void invalidDividend(){
+        assertThrows(DividendIsZeroException.class, () -> {
+            Calculator.divide(0,0);
+        });
+    }
 }
